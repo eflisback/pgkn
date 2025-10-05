42855a2884af1517d8397fc732a23659063f4d78
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2025-10-06 00:06:26 +0200

    Random home-page quote

--- src/main/scala/pgkn/pages/Home.scala
+++ src/main/scala/pgkn/pages/Home.scala
@@ -3,15 +3,25 @@ package pgkn.pages
 import com.raquo.laminar.api.L.*
 import com.raquo.waypoint.*
 import pgkn.components.NavHeader
+import scala.util.Random
 
 object Home:
+  private val quotes = List(
+    ("Sekvens, Alternativ, Repetition, Abstraktion.", "Björn Regnell"),
+    ("Koda, koda, koda!", "Björn Regnell"),
+    ("WUUUUAAAAH!", "Mutanten"),
+    ("Det är jag som är döden.", "Skräpsamlaren")
+  )
+
   def apply(router: Router[pgkn.Page]): HtmlElement =
+    val quote = Random.shuffle(quotes).head
+
     mainTag(
       className := "home-page",
       NavHeader(router),
       div(
         className := "home-page-content",
-        h1("Koda, koda, koda!"),
-        span("- Björn Regnell")
+        h1(s"\"${quote._1}\""),
+        span(s"- ${quote._2}")
       )
     )
