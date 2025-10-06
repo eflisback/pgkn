c271ca045fd8ca4cfd33dabb6e5f28a5cd057bf9
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2025-10-06 19:27:48 +0200

    Version 1.0.0 with version tag

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,2 @@
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.20.1")
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.12.0")
---
+++ src/main/scala/pgkn/components/Footer.scala
@@ -0,0 +1,16 @@
+package pgkn.components
+
+import com.raquo.laminar.api.L.*
+import pgkn.BuildInfo
+
+object Footer:
+  def apply(): HtmlElement =
+    footerTag(
+      className := "app-footer",
+      a(
+        href := "https://github.com/eflisback/pgkn",
+        target := "_blank",
+        SvgIcon("/icons/git.svg"),
+        span(s"v${BuildInfo.version}")
+      )
+    )
